bbaa0659a6a60064b34d13550971561ef4ce8eaf
szymonkawalerski <123446171+szymonkawalerski@users.noreply.github.com>
2023-02-21 11:12:40 +0100

    Add files via upload

---
+++ quizGame/data.py
@@ -0,0 +1,15 @@
+question_data = [
+    {"text": "Stolicą Niemiec jest Berlin.", "answer": "True"},
+    {"text": "Samica anakondy może osiągnąć największą długość 3m.", "answer": "False"},
+    {"text": "Człowiek ma 33-34 kręgi.", "answer": "True"},
+    {"text": "Batman nazywa się Bruce Wayne.", "answer": "True"},
+    {"text": "Wyprodukowano 5 filmów fabularnych Spider-mana.", "answer": "True"},
+    {"text": "Senat składa się z 70 senatorów..", "answer": "False"},
+    {"text": "Aby zostać prezydentem należy ukończyć 35 lat.", "answer": "True"},
+    {"text": "Python został napisany w Javie.", "answer": "False"},
+    {"text": "Python jest językiem dynamicznie typowanym.", "answer": "True"},
+    {"text": "World of Warcraft jest grą Blizzarda.", "answer": "True"},
+    {"text": "Najpopularniejszym językiem programowania jest PHP.", "answer": "False"},
+    {"text": "Czekolada może zabić psa.", "answer": "True"}
+]
+
